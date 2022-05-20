cf713f97e8daec6480dcc0c67b5ce8520d3559fe
akida15 <105627917+akida15@users.noreply.github.com>
2022-05-20 18:42:00 +0200

    Add files via upload

---
+++ plugins/unbanchat.js
@@ -0,0 +1,10 @@
+let handler = async (m, { conn }) => {
+  global.db.data.chats[m.chat].isBanned = false
+  m.reply('تم')
+}
+handler.help = ['تكلم']
+handler.tags = ['owner']
+handler.command = /^تكلم$/i
+handler.owner,handler.mods = true
+
+module.exports = handler
